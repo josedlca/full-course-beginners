d6da95852439cf88413bf59e728148cf1526705a
josedlca <jose.dlcruz.ayala@gmail.com>
2021-02-05 16:32:44 -0500

    setState react

--- src/App.js
+++ src/App.js
@@ -15,16 +15,27 @@ class App extends Component{
   constructor(){
     super()
     this.state={
-      isLoggedIn: false
+      count: 0
     }
+
+    this.handleClick = this.handleClick.bind(this)
   }
+
+  handleClick(){
+    this.setState(
+      prevState=>{
+        return{
+          count: prevState.count + 1
+        }
+      }
+    )
+  }
+
   render(){
     return(
       <div>
-        <h1>You are currently logged (
-          {this.state.isLoggedIn ? 'in':'out'}
-        )
-        </h1>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
       </div>
     )
   }
