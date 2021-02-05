365bf0662855d67953579f8e1086b5901753d7c7
josedlca <jose.dlcruz.ayala@gmail.com>
2021-02-05 13:36:48 -0500

    class challenge two done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{Component} from 'react'
 import './App.css';
 import Header from './components/Header'
 import Greeting from './components/Greeting'
@@ -11,12 +11,19 @@ import Greeting from './components/Greeting'
 //   );
 // }
 
-class App extends React.Component{
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      name: 'Jose',
+      age: 21
+    }
+  }
   render(){
     return(
       <div>
-        <Header username='Jose De La Cruz'/>
-        <Greeting/>
+        <h1>{this.state.name}</h1>
+        <h3>{this.state.age} years old</h3>
       </div>
     )
   }
